2e48c7694ffc9eb364b285c1b27dcc544f8b481d
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-08 09:46:20 -0600

    created validation test for notifications

--- message_service/app/models/notification.rb
+++ message_service/app/models/notification.rb
@@ -1,14 +1,3 @@
-class Notification < ApplicationRecord
-  require 'rails_helper'
-
-  RSpec.describe Notification, type: :model do 
-    describe 'creation' do
-      it 'can be created' do
-        notification = Notification.create(phone: "555-555-5555", body: "My message", source_app: "some_app")
-        expect(notification).to be_valid
-      end
-    end
-  end
-
-
+class Notification < ActiveRecord::Base
+    validates_presence_of :phone, :body, :source_app
 end
--- message_service/spec/models/notification_spec.rb
+++ message_service/spec/models/notification_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  describe 'creation' do
-    it 'can be created' do
-      notification = FactoryGirl.build_stubbed(:notification)
-      expect(notification).to be_valid
+    describe 'creation' do
+        it 'can be created' do
+            notification = FactoryGirl.build_stubbed(:notification)
+            expect(notification).to be_valid
+        end
     end
-  end
+
+    describe 'validations' do
+        it 'can be create if valid' do 
+            notification = FactoryGirl.build_stubbed(:notification)
+            notification.phone = nil
+            notification.body = nil
+            notification.source_app = nil
+            expect(notification).to_not be_valid
+        end
+    end
 end
